e68f1759e7b72946e09dbf741e1eff3b9d4c70f0
kientrdoan <dr.kientrdoan@gmail.com>
2023-05-10 00:36:24 +0700

    update

--- CuoiKy/Climbing.py
+++ CuoiKy/Climbing.py
@@ -1,6 +1,13 @@
+"""
+    Trương Thái Hoàng - N20DCCN019
+    Đoàn Trung Kiên - N20DCCN027
+    Lê Ngọc Tuấn Kiệt - N20DCCN028
+"""
+
 from copy import deepcopy
 import time
 start_time = time.time()
+
 def read_file(path):
     class_available = []
     with open(path, "r") as f:
@@ -12,6 +19,7 @@ def read_file(path):
             class_available.append(data[i])
     return class_available
 
+#Kiem tra lop co the hoc vao tiet bat buoc
 def checkMandatory(mandatory_day, class_available):
     for i in range(1, len(mandatory_day)):
         for j in range(1, len(mandatory_day[i])):
@@ -19,98 +27,131 @@ def checkMandatory(mandatory_day, class_available):
                 return False, mandatory_day[i][0], mandatory_day[0][j - 1]
     return True, None, None
 
+#teacher, class, period la index
 def gradeCal(teacher_class, mandatory_day, schedule, teacher_available, class_available, period, Class, teacher, currentTeacher, countMan):
+    
     grade=0
-    says=""
-    if int(teacher_class[teacher][Class])<1:
-        grade-=1000
+    #Tong So Tiet Giao Vien Day Cho Tung Lop
+    if int(teacher_class[teacher][Class]) <1 :
+        grade -= 10000
     else:
-        grade+=int(teacher_class[teacher][Class])
+        grade+=(int(teacher_class[teacher][Class]))*100
+
+    #Tiet khong bat buoc phai hoc
     if mandatory_day[period][Class]=="0" and countMan>0:
-        grade-=1000
+        grade-=10000
+
+    #Tiet Ma Lop Khong The Hoc
     if class_available[period][Class]=="0":
-        grade-=1000
+        grade-=10000
+    
+    #GV Khong The Day 
     if teacher_available[period][teacher]=="0":
-        grade-=1000
+        grade-=10000
+    
+    #Giao Vien Day Hai Lop Cung Tiet
     try:
         schedule[period].index(teacher)
-        grade-=1000
+        grade-=10000
     except:
         None
+
+    #So giáo viên có thể dạy cho lớp
     countAvailableTeachers=0
     for i in range(1, len(teacher_class)):
         if int(teacher_class[i][Class])>0:
             countAvailableTeachers+=1
+            #Neu Ma GV Da Co Tiet
             try:
                 schedule[period].index(i)
                 countAvailableTeachers-=1
             except:
                 None 
-    if currentTeacher!="X":
-        grade-=100
-    grade+=len(teacher_available[0])-countAvailableTeachers-1
+
+    if currentTeacher != "X":
+        grade-=7000
+    
+    #Uu tien giao vien con nhieu tiet hon
+    for i in range(1, len(teacher_class[1])):
+        grade+=int(teacher_class[teacher][i])
+
+    grade+=(len(teacher_available[0])-countAvailableTeachers-1)*1000
     return grade
 
 def convert(schedule, teachers):
     for i in range(1, len(schedule)):
         for j in range(1, len(schedule[i])):
-            if schedule[i][j]=="X": continue
-            schedule[i][j]=teachers[schedule[i][j]-1]
+            if schedule[i][j]=="X": 
+                continue
+            schedule[i][j] = teachers[schedule[i][j]-1]
     return schedule
 
-def addTeacher(schedule, class_available, teacher_available, teacher_class, mandatory_day, periodNum, countMan):
-    biggestGrade = -10000
-    biggestTeacher=0
-    biggestClass=0
-    biggestPeriod=0
-    for period in range(1, len(mandatory_day)):
-        for Class in range(1, len(mandatory_day[period])):
-            for teacher in range(1, len(teacher_available[1])):
-                if teacher==schedule[period][Class]: continue
-                currentTeacher=schedule[period][Class]
-                currentGrade = gradeCal(teacher_class, mandatory_day, schedule, teacher_available, class_available, period, Class, teacher, currentTeacher, countMan)
+def addTeacher(schedule, class_available, teacher_available, teacher_class, mandatory_day, periodNum, countMan, grade):
+    biggestGrade = grade
+    biggestTeacher=0 #Index
+    biggestClass=0 #Index
+    biggestPeriod=0 #Index
+
+    for period in range(1, len(schedule)): #Duyet qua tat ca cac tiet
+        for Class in range(1, len(schedule[period])): #Duyet qua tat ca cac lop
+            for teacher in range(1, len(teacher_available[1])): #Duyet qua tung giao vien
+                if teacher == schedule[period][Class]: 
+                    continue
+                currentTeacher = schedule[period][Class] #Lay GV
+                currentGrade = grade + gradeCal(teacher_class, mandatory_day, schedule, teacher_available, class_available, period, Class, teacher, currentTeacher, countMan)
                 if currentGrade>biggestGrade:
-                    biggestGrade=currentGrade
-                    biggestClass=Class
-                    biggestPeriod=period
-                    biggestTeacher=teacher
-    if biggestTeacher==0:
-        print("Lịch xếp chưa hoàn thành:")
-        schedule=convert(schedule, teacher_available[0])
+                    biggestGrade = currentGrade
+                    #Lay Vi Tri
+                    biggestClass = Class
+                    biggestPeriod = period
+                    biggestTeacher = teacher
+    if biggestTeacher == 0:
+        for i in range(1, len(teacher_class)):
+            for j in range(1, len(teacher_class[i])):
+                if int(teacher_class[i][j]) != 0:
+                    #print("Lịch xếp chưa hoàn thành:")
+                    schedule=convert(schedule, teacher_available[0])
+                    for i in range(len(schedule)):
+                        print(schedule[i])
+                    file = open("out_put.csv", "w", encoding= "utf-8")
+                    out_put(schedule, file)
+                    #print("Các tiết bị dư ra là:")
+                    for i in range(len(teacher_class)):
+                        print(teacher_class[i])
+                    file.write("\n\nCac, tiet, bi, du, ra, la:\n")
+                    out_put(teacher_class, file)
+                    exit()
+        print("Xep lich xong!")
+        convert(schedule, teacher_available[0])
         for i in range(len(schedule)):
             print(schedule[i])
         file = open("out_put.csv", "w", encoding= "utf-8")
         out_put(schedule, file)
-        print("Các tiết bị dư ra là:")
-        for i in range(len(teacher_class)):
-            print(teacher_class[i])
-        file.write("\n\nCac, tiet, bi, du, ra, la:\n")
-        out_put(teacher_class, file)
         exit()
+        
     else:
-        if schedule[biggestPeriod][biggestClass]=="X":
+        if schedule[biggestPeriod][biggestClass] == "X":
             periodNum-=1
         else:
-            teacher_class[biggestClass][schedule[biggestPeriod][biggestClass]] = int(teacher_class[biggestClass][biggestTeacher])+1
-        if mandatory_day[biggestPeriod][biggestClass]=="1":
+            teacher_class[schedule[biggestPeriod][biggestClass]][biggestClass] = int(teacher_class[biggestTeacher][biggestClass])+1
+        if mandatory_day[biggestPeriod][biggestClass] == "1":
             countMan-=1
-        if biggestGrade<0:
-            print("Here")
-        teacher_class[biggestClass][biggestTeacher]= int(teacher_class[biggestClass][biggestTeacher])-1
-        schedule[biggestPeriod][biggestClass]=biggestTeacher
-    # if periodNum==252:
-    #     print(schedule[biggestPeriod][biggestClass], biggestTeacher, biggestGrade, biggestClass, biggestPeriod)
-    #     print()
-    return schedule, teacher_class, periodNum, countMan
+        #So tiet giao vien day cho mot lop
+        teacher_class[biggestTeacher][biggestClass]= int(teacher_class[biggestTeacher][biggestClass])-1
+        #The Vi Tri Cua GV
+        schedule[biggestPeriod][biggestClass] = biggestTeacher
+    return schedule, teacher_class, periodNum, countMan, biggestGrade
 
 
 def climbing(schedule, class_available, teacher_available, teacher_class, mandatory_day):
+    #Tong so tiet giao vien day
     periodNum=0
     for i in range(1, len(teacher_class)):
         for j in range(1, len(teacher_class[i])):
             periodNum += int(teacher_class[i][j])
-    countMan = 0
-    countAvail=0
+    
+    countMan = 0 #So tiet bat buoc
+    countAvail=0 #So tiet lop co the hoc
     for i in range(1, len(class_available)):
         for j in range(1, len(class_available[1])):
             if class_available[i][j]=="1":
@@ -120,11 +161,15 @@ def climbing(schedule, class_available, teacher_available, teacher_class, mandat
     if countAvail < periodNum:
         print("Số tiết cần dạy nhiều hơn số tiết các lớp có thể học")
         exit()
-    print(countMan)
-    while periodNum > 0:
+    grade=0
+    for i in range(1, len(teacher_class)):
+        for j in range(1, len(teacher_class[i])):
+            grade-= int(teacher_class[i][j])*1000
+    
+    while True:
         print("So tiet chua xep: ",periodNum)
-        schedule, teacher_class, periodNum, countMan = addTeacher(schedule, class_available, teacher_available, teacher_class, mandatory_day, periodNum, countMan)
-    return schedule, teacher_class
+        schedule, teacher_class, periodNum, countMan, grade = addTeacher(schedule, class_available, teacher_available, teacher_class, mandatory_day, periodNum, countMan, grade)
+    # return schedule, teacher_class
 
 def out_put(schedule, file):
     # print(schedule)
@@ -137,7 +182,7 @@ def out_put(schedule, file):
     for i in range(1, len(schedule)):
         line = ""
         for j in range(len(schedule[i])):
-            line += schedule[i][j] + ","
+            line += str(schedule[i][j]) + ","
         
         line += "\n"
         file.write(line)
@@ -148,10 +193,11 @@ if __name__ == "__main__":
     teacher_available = read_file("Teacher-Available.txt")
     teacher_class = read_file("Teacher-Class.txt")
     canSchedule=True
-    canSchedule, cannotPeriod, cannotClass=checkMandatory(mandatory_day, class_available)
+    canSchedule, cannotPeriod, cannotClass = checkMandatory(mandatory_day, class_available)
     if(not canSchedule):
         print("Lớp "+ str(cannotClass) + " không thể học vào tiết "+str(cannotPeriod)+" theo lịch học bắt buộc")
     else:
+        #Khoi tao schedule
         schedule=[class_available[0]]
         for i in range(48):
             date=[]
@@ -160,11 +206,6 @@ if __name__ == "__main__":
             for j in range(len(teacher_class[0])):
                 date+=["X"]
             schedule+=[date]
+        
+        #Bat dau xep thoi khoa bieu
         schedule, teacher_class = climbing(schedule, class_available, teacher_available, teacher_class, mandatory_day)
-        print("Xep lich xong!")
-        convert(schedule, teacher_available[0])
-        for i in range(len(schedule)):
-            print(schedule[i])
-        # print("Process finished --- %s seconds ---" % (time.time() - start_time))
-        file = open("out_put.csv", "w", encoding= "utf-8")
-        out_put(schedule, file)
